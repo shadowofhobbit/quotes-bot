3d6faf1893db07d2022b4505a1dd2df168c8668e
Iuliia <iuliiaponomareva@gmail.com>
2020-10-25 17:08:31 +0300

    Refactor

--- src/main/java/com/example/quotesbot/TelegramService.java
+++ src/main/java/com/example/quotesbot/TelegramService.java
@@ -1,8 +1,5 @@
 package com.example.quotesbot;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -19,16 +16,16 @@ import java.util.logging.Logger;
 
 @Service
 public class TelegramService {
+    public static final String QUOTE_COMMAND = "/quote";
+    public static final String START_COMMAND = "/start";
+    public static final String DEFAULT_ANSWER = ":-)";
     @Value("${token}")
     private String token;
-    private long offset = 0;
-    private static final int TIMEOUT = 100;
     private final QuotesRepository quotesRepository;
-    private final ObjectMapper mapper = new ObjectMapper()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     private static final String API_BASE_URL = "https://api.telegram.org/bot";
     @Value("${host}")
     private String path;
+    public static final String GREETING = "Hi!";
 
     @Autowired
     public TelegramService(QuotesRepository quotesRepository) {
@@ -48,75 +45,23 @@ public class TelegramService {
                 .whenComplete((stringHttpResponse, throwable) -> {
                     Logger.getGlobal().info(stringHttpResponse.body());
                     if (throwable != null) {
-                        Logger.getGlobal().severe("error creating webhook");
+                        Logger.getGlobal().severe("Error creating webhook");
                         throwable.printStackTrace();
                     }
                 });
     }
 
-
-    public void checkForUpdates() {
-        var httpClient = HttpClient.newHttpClient();
-        var uri = URI.create(API_BASE_URL + token
-                + "/getUpdates?allowed_updates=message&offset=" + offset
-                + "&timeout=" + TIMEOUT);
-        var request = HttpRequest.newBuilder()
-                .GET()
-                .uri(uri)
-                .build();
-        httpClient.sendAsync(request,
-                HttpResponse.BodyHandlers.ofString())
-                .thenAccept(stringHttpResponse -> {
-                    if (stringHttpResponse.statusCode() >= 500) {
-                        try {
-                            Thread.sleep(10000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    } else {
-                        parseUpdate(stringHttpResponse.body());
-                    }
-                })
-                .whenCompleteAsync((unused, throwable) -> {
-                    if (throwable != null) {
-                        throwable.printStackTrace();
-                    }
-                    this.checkForUpdates();
-                });
-    }
-
-    private void parseUpdate(String x) {
-        try {
-            var updates = mapper.readValue(x, Updates.class);
-            var result = updates.getResult();
-            if (result.length > 0) {
-                var lastUpdate = result[result.length - 1];
-                offset = lastUpdate.getUpdateId() + 1;
-                handleMessages(result);
-            }
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    private void handleMessages(Update[] updates) {
-        for (Update update: updates) {
-            handleUpdate(update);
-        }
-    }
 
     void handleUpdate(Update update) {
         var receivedText = update.getMessage().getText();
         var chatId = update.getMessage().getChat().getId();
-        if (receivedText.equals("/quote")) {
+        if (receivedText.equals(QUOTE_COMMAND)) {
             var quote = quotesRepository.getQuote();
             sendMessage(chatId, quote);
-        } else if (receivedText.equals("/start")) {
-            var greeting = "Hi!";
-            sendMessage(chatId, greeting);
+        } else if (receivedText.equals(START_COMMAND)) {
+            sendMessage(chatId, GREETING);
         } else {
-            sendMessage(chatId, ":-)");
+            sendMessage(chatId, DEFAULT_ANSWER);
         }
     }
 
